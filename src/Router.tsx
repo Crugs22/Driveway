71904bc2e0e868aff1cd3a482e1d4932c70f2f90
Caio Moura <caiomouraa@hotmail.com>
2024-11-02 00:19:06 -0300

    First commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,11 +10,11 @@ import { Login } from './Pages/Users/Login'
 export function Router(){
     return (
         <Routes>
-            <Route path="https://crugs22.github.io/Driveway/" element={<Home />} />
-            <Route path="https://crugs22.github.io/Driveway/payment" element={<Payment />} />
-            <Route path="https://crugs22.github.io/Driveway/products" element={<Products />} />
-            <Route path="https://crugs22.github.io/Driveway/location" element={<Location />} />
-            <Route path="https://crugs22.github.io/Driveway/user/Login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/location" element={<Location />} />
+            <Route path="/user/Login" element={<Login />} />
         </Routes>
     )
 } 
